f4fbb218b6102a781210d5ef64edd773d867995a
code_daf <codedaf1@gmail.com>
2024-06-17 14:04:13 +0930

    4 commit

---
+++ Exercise_1_Basic_Calculator.py
@@ -0,0 +1,29 @@
+"""
+Example #1: Basic Calculator
+"""
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide by zero"
+
+# Prompt the user to enter the values
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+# Display the results of the operations
+print("Sum:", suma(a, b))
+print("Subtraction:", resta(a, b))
+print("Multiplication:", multiplicacion(a, b))
+print("Division:", division(a, b))
+
+
